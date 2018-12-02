e1923eff7ad91e85f2e7d7faaf0ade942079b877
Michał Błotniak <miblotniak@gmail.com>
2018-12-02 20:58:01 +0100

    Remove comment

--- domain-blacklist.py
+++ domain-blacklist.py
@@ -1,35 +1,3 @@
-# Copyright 2013 <Your Name Here>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at:
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-A skeleton POX component
-
-You can customize this to do whatever you like.  Don't forget to
-adjust the Copyright above, and to delete the Apache license if you
-don't want to release under Apache (but consider doing so!).
-
-Rename this file to whatever you like, .e.g., mycomponent.py.  You can
-then invoke it with "./pox.py mycomponent" if you leave it in the
-ext/ directory.
-
-Implement a launch() function (as shown below) which accepts commandline
-arguments and starts off your component (e.g., by listening to events).
-
-Edit this docstring and your launch function's docstring.  These will
-show up when used with the help component ("./pox.py help --mycomponent").
-"""
-
 # Import some POX stuff
 from pox.core import core                     # Main POX object
 import pox.openflow.libopenflow_01 as of      # OpenFlow 1.0 library
